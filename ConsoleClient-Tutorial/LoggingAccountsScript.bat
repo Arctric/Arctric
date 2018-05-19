a2078a830f58d0dc1e6c0a44d8350802b987b3a1
Ramon Alberto <32604639+Arctric@users.noreply.github.com>
2018-05-18 22:50:31 -0600

    Add files via upload

---
+++ ConsoleClient-Tutorial/LoggingAccountsScript.bat
@@ -0,0 +1,22 @@
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
+start /min MinecraftClient.exe username password serverip
+timeout 5
